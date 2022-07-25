713080900e9c7061565dbddca4d1b2d1a598805a
paulcervov <paulcervov@gmail.com>
2022-07-26 00:40:08 +0300

    Implement general logic

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
+const axios = require('axios').default;
+const { pipeline } = require('stream/promises');
+const csv = require('csv');
+const fs = require('fs');
 require('dotenv').config();
 
-console.log(process.env.API_KEY_GEOCODER);
+async function bootstrap() {
 
-// TO DO the rest logic
+    await pipeline(
+        fs.createReadStream('centers_demo.csv'),
+        csv.parse({
+            columns: true,
+            delimiter: ';',
+            trim: true
+        }),
+        csv.transform(async (input, done) => {
+            const url = encodeURI(`https://geocode-maps.yandex.ru/1.x/?apikey=${process.env.API_KEY_GEOCODER}&format=json&geocode=${input.city}`);
+            const {data: {response: {GeoObjectCollection: {featureMember}}}} = await axios(url);
+            const city_coordinates = featureMember[0].GeoObject.Point.pos;
+            done(null, {...input, city_coordinates})
+        }),
+        csv.stringify({header: true}),
+        fs.createWriteStream('centers_demo_processed.csv')
+    );
+    console.log('Done 🍻');
+}
+
+bootstrap().catch(console.error);
