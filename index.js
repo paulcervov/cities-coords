cd8838982e99b4b2252409967342406b2d8b1027
paulcervov <paulcervov@gmail.com>
2022-07-26 11:43:01 +0300

    Add caching of city coordinates

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ require('dotenv').config();
 
 async function bootstrap() {
 
+    const cityCoordinatesCache = new Map();
+
     await pipeline(
         fs.createReadStream('centers_demo.csv'),
         csv.parse({
@@ -14,9 +16,20 @@ async function bootstrap() {
             trim: true
         }),
         csv.transform(async (input, done) => {
+
+            // try to get coordinates from cache
+            if(cityCoordinatesCache.has(input.city)){
+                done(null, {...input, city_coordinates: cityCoordinatesCache.get(input.city)})
+            }
+
+            // request to geocoder API
             const url = encodeURI(`https://geocode-maps.yandex.ru/1.x/?apikey=${process.env.API_KEY_GEOCODER}&format=json&geocode=${input.city}`);
             const {data: {response: {GeoObjectCollection: {featureMember}}}} = await axios(url);
             const city_coordinates = featureMember[0].GeoObject.Point.pos;
+
+            // update cache
+            cityCoordinatesCache.set(input.city, city_coordinates);
+
             done(null, {...input, city_coordinates})
         }),
         csv.stringify({header: true}),
